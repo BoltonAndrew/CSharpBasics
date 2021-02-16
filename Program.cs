a03f3acff92e32cd61148cbcbbd79efb04ed1f84
BostonWafro <wafrogaming@gmail.com>
2021-02-16 16:59:44 +0000

    made basic expenditure console app

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine("Would you like me to calculate your spare cash? Y/N");
+            string response = Console.ReadLine();
+            if (response == "y")
+            {
+                Console.WriteLine($"{name}, how much are your current expenses?");
+                double expenses = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("And what is your total income?");
+                double income = Convert.ToDouble(Console.ReadLine());
+                double total = Math.Round(income - expenses, 2);
+                Console.WriteLine($"{name}, you have £{total} spare cash");
+            } 
+            else 
+            {
+                Console.WriteLine("Fair enough");
+            }
         }
     }
 }
